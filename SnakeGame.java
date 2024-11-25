991d5c788aa128a9927b427649dc505e99e34002
MaNa-java <03081230003@student.uph.edu>
2024-11-25 14:40:11 +0700

    SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,13 +1,13 @@
 package Snake;
 
-import java.awt.*; //ni si biar kita bisa gunakan si Graphics utk atur warna font
-import java.awt.event.*; //klo ni biar kita bisa pake action listener sama key listener, supaya pas kita main tuh bisa pake keyboard
-import java.util.ArrayList; //storing the segments of the snake's body
-import java.util.Random; //getting random x and y values to place the food on the screen
-import javax.swing.*; //ni si buat kita bisa menggunakan JPanel ama JFrame, soalny klo g import g bakal bisa kita pake
+import java.awt.*; 
+import java.awt.event.*; 
+import java.util.ArrayList; 
+import java.util.Random; 
+import javax.swing.*; 
 
 public class SnakeGame extends JPanel implements ActionListener, KeyListener{
-    private class Tile { //private supaya hny SnakeGame yang bisa akses class nihh
+    private class Tile { 
         int x;
         int y;
 
@@ -40,9 +40,9 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
         this.boardHeight = boardHeight;
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.black);
-        addKeyListener(this); //nih biar kita bisa menggunakan keyboard sbg perintah biar si ular bisa gerak
-        setFocusable(true); // nih biar dia bisa paham, bisa jalankan perintah
-
+        addKeyListener(this); 
+        setFocusable(true); 
+        
         snakeHead = new Tile(5, 5);
         snakeBody = new ArrayList<Tile>();
 
@@ -52,10 +52,6 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
 
         gameLoop = new Timer(100, this);
         gameLoop.start();
-        //100 adalah interval waktu dalam milidetik (0,1 detik), artinya setiap 0,1 detik, game akan:
-        //Menggerakkan ular.
-        //Memeriksa kondisi (apakah makanan dimakan, tabrakan terjadi, dll).
-        //Menggambar ulang layar (repaint).
 
         velocityX = 0;//ni artinya pas awal mulai game, kecepatannya 0 (g bergerak)
         velocityY = 0;
@@ -67,17 +63,12 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
     }
 
     public void draw(Graphics g) {
-        //Grid
-//        for (int i = 0; i < boardWidth/tileSize; i++){
-//            g.drawLine(i*tileSize, 0, i*tileSize, boardHeight);
-//            g.drawLine(0, i*tileSize, boardWidth, i*tileSize);
-//        }
         if (!gameStarted) {
             // Tampilkan pesan awal sebelum game dimulai
             g.setColor(Color.white);
             g.setFont(new Font("Arial", Font.BOLD, 24));
             g.drawString("Press 'Space Bar' to Start", 150, 280);
-            return; // Keluar dari metode draw supaya snake dan food tidak digambar
+            return; 
         }
         if (!gameOver) {
             //Snake Head
@@ -90,7 +81,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
                 Tile snakePart = snakeBody.get(i);
                 //g.fillRect(snakePart.x*tileSize, snakePart.y*tileSize, tileSize, tileSize);
                 g.fill3DRect(snakePart.x * tileSize, snakePart.y * tileSize, tileSize, tileSize, true);
-            }//apparently klo lu e snake body susunannya snake head lalu food lalu snake body, warna bodyny jd merah TT
+            }
 
 
             //Food
@@ -115,12 +106,12 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
     }
 
     public void placeFood() {
-        food.x = random.nextInt(boardWidth/tileSize); //jd food ny tuh bakal di posisi yg random antara 0-24 krn 600/25 = 24
-        food.y = random.nextInt(boardHeight/tileSize); //tiap kali run bakal beda" posisi food ny
+        food.x = random.nextInt(boardWidth/tileSize); 
+        food.y = random.nextInt(boardHeight/tileSize); 
     }
 
     public boolean collision(Tile tile1, Tile tile2) {
-        return tile1.x == tile2.x && tile1.y == tile2.y; //utk cek apakah 2 tile (snake) tabrak ato g
+        return tile1.x == tile2.x && tile1.y == tile2.y; 
     }
 
     public void move() {
@@ -130,15 +121,15 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
             placeFood();
         }
 
-        //snake body (nihh supaya pas dia makan nnti body ny bakal nyambung ke head, bukan ttp di posisi food sblmnya en cuma modal ganti warna)
+        //snake body 
         for (int i = snakeBody.size()-1; i >= 0; i--) {
             Tile snakePart = snakeBody.get(i);
-            if (i==0) { //nih (utk tile pertama setelah kepala) biar jika dia kek member pertama body ny nnti dia bakal asik copas posisi head ny gitu biar terkesan ngikut kepala
+            if (i==0) { 
                 snakePart.x = snakeHead.x;
                 snakePart.y = snakeHead.y;
             }
             else{
-                Tile prevSnakePart = snakeBody.get(i-1); //utk tile selanjutnya, mulai dr ke 2 stlh kepala
+                Tile prevSnakePart = snakeBody.get(i-1); 
                 snakePart.x = prevSnakePart.x;
                 snakePart.y = prevSnakePart.y;
             }
@@ -183,11 +174,10 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
         move();
         repaint();
         if (gameOver) {
-            gameLoop.stop();//ni buat setiap 100 milisecond sesuai timer, nnti bakal asik repaint (draw ulang"), kecuali dia gameover
+            gameLoop.stop();
         }
     }
 
-    //sedangkan klo nih butuh krn w lang hny butuh interaksi dari panah di keyboard buat main game nihh
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_UP && velocityY !=1) {
@@ -206,20 +196,18 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
             velocityX = 1;
             velocityY = 0;
         }
-        //&& nih semua fungsinya supaya klo dia udh ke kiri dia g bisa balek ke arah kanan gitu di row yg sama hrs naik ato turun dlu br bisa balek arah kiri/kanan
 
         else if (e.getKeyCode() == KeyEvent.VK_SPACE && !gameStarted) {
-            gameStarted = true;  // Game dimulai
-            gameLoop.start();    // Timer dimulai
-            repaint();           // Menggambarkan ulang agar snake dan food muncul
+            gameStarted = true;  
+            gameLoop.start();    
+            repaint();           
         }
 
         else if (e.getKeyCode() == KeyEvent.VK_ENTER && gameOver) {
-            resetGame();  // Panggil reset game saat space bar ditekan
+            resetGame();  
         }
     }
-
-    //nih kedua g perlu si, hny buat mendefine fungsi key listener supaya bisa jalan ja
+    
     @Override
     public void keyTyped(KeyEvent e) {}
     @Override
